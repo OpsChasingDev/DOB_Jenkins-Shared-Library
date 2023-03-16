9195474ed655c6fa0ef11ca87a154edb88d69f48
Robert Stapleton <robert@stapleton-family.net>
2023-03-16 16:48:37 -0400

    params for image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 #!/usr/hbin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'Docker Hub repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh 'docker build -t opschasingdev/dockerhub:maven-app-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push opschasingdev/dockerhub:maven-app-2.0'
+        sh "docker push $imageName"
 }
