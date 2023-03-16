43e3fbde309e83ba9ab3a5c4a1c0edf9f9ebf1fb
Robert Stapleton <robert@stapleton-family.net>
2023-03-16 16:59:02 -0400

    finished class and calling class from groovy script

--- src/com.example/Docker.groovy
+++ src/com.example/Docker.groovy
@@ -3,4 +3,18 @@ package com.example
 
 class Docker implements Serializable {
 
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.withCredentials([script.usernamePassword(credentialsId: 'Docker Hub repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
 }
